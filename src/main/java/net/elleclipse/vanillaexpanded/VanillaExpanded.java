5c7ebe49f9817d715f727fb6d9be0713667c9e34
ElleBurt <raxrevolt@gmail.com>
2024-07-27 14:00:06 +0100

    added withered bones and amethyst_upgrade_smithing_template to loot pools

--- src/main/java/net/elleclipse/vanillaexpanded/VanillaExpanded.java
+++ src/main/java/net/elleclipse/vanillaexpanded/VanillaExpanded.java
@@ -1,16 +1,35 @@
 package net.elleclipse.vanillaexpanded;
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
 import net.elleclipse.vanillaexpanded.item.ModItemGroups;
 import net.elleclipse.vanillaexpanded.item.ModItems;
+import net.elleclipse.vanillaexpanded.util.ModLootTableModifier;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.SimpleFabricLootTableProvider;
+import net.minecraft.item.Items;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.context.LootContextTypes;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.InvalidIdentifierException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.function.BiConsumer;
 
 public class VanillaExpanded implements ModInitializer {
 
 	public static final String MOD_ID = "vanilla-expanded";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Identifier TEST_CHEST = Identifier.of("vanilla-expanded", "chests/amethyst_geode_loot_1");
 
 	@Override
 	public void onInitialize() {
@@ -19,5 +38,7 @@ public class VanillaExpanded implements ModInitializer {
 		ModItemGroups.registerItemGroups();
 		ModItems.registerModItems();
 
+		ModLootTableModifier.modifyLootTable();
 	}
+
 }
---
+++ src/main/java/net/elleclipse/vanillaexpanded/util/ModLootTableModifier.java
@@ -0,0 +1,53 @@
+package net.elleclipse.vanillaexpanded.util;
+
+
+import net.elleclipse.vanillaexpanded.item.ModItems;
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.minecraft.block.StructureBlock;
+import net.minecraft.block.entity.ChestBlockEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.LootTables;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LootTableEntry;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Identifier;
+
+
+public class ModLootTableModifier {
+
+    private static final RegistryKey<LootTable> ABANDONED_MINESHAFT_MINECART_CHEST_ID = LootTables.ABANDONED_MINESHAFT_CHEST;
+    private static final RegistryKey<LootTable> WITHER_SKELETON_ID = EntityType.WITHER_SKELETON.getLootTableId();
+
+    public static void modifyLootTable(){
+        LootTableEvents.MODIFY.register((key, tableBuilder, source, resourceManager) -> {
+            if(source.isBuiltin() && WITHER_SKELETON_ID.equals(key)){
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.5f))
+                        .with(ItemEntry.builder(ModItems.WITHERED_BONE))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f,2.0f)).build());
+
+                tableBuilder.pool(poolBuilder);
+            }
+
+            if(source.isBuiltin() && ABANDONED_MINESHAFT_MINECART_CHEST_ID.equals(key)){
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.5f))
+                        .with(ItemEntry.builder(ModItems.AMETHYST_UPGRADE_SMITHING_TEMPLATE))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f,2.0f)).build());
+
+                tableBuilder.pool(poolBuilder);
+            }
+        });
+    }
+
+}
